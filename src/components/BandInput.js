e3bfb33209536effa88f14e6db96a795a4f2e011
Paul-Cl-ark <clark.paul123@gmail.com>
2019-01-27 15:46:57 +0000

    works as it should but doesn't pass the last test...

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,13 +2,31 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
-  render() {
-    return(
-      <div>
-        Band Input
-      </div>
-    )
-  }
+	state = {
+		name: ''
+	}
+
+	handleSubmit = event => {
+		event.preventDefault()
+		this.props.addNewBand(this.state)
+		this.setState({ name: '' })
+	}
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={event => this.handleSubmit(event)}>
+					<label>Cr8 ur b@nd: </label>
+					<input
+						type="text"
+						value={this.state.name}
+						onChange={event => this.setState({ name: event.target.value })}
+					/>
+					<input type="submit" />
+				</form>
+			</div>
+		)
+	}
 }
 
 export default BandInput
